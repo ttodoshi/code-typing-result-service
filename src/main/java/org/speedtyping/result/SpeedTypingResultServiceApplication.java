b56d2dbfe016de2c387e6207176fef91fbde5120
todoshi <ttodoshi@users.noreply.github.com>
2024-03-04 01:32:21 +0300

    feat: added session results migration when user is authorizing using RabbitMQ

--- src/main/java/org/speedtyping/result/SpeedTypingResultServiceApplication.java
+++ src/main/java/org/speedtyping/result/SpeedTypingResultServiceApplication.java
@@ -3,6 +3,7 @@ package org.speedtyping.result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
---
+++ src/main/java/org/speedtyping/result/config/MessageConverterConfig.java
@@ -0,0 +1,14 @@
+package org.speedtyping.result.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConverterConfig {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
